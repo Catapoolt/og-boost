7c591580ad7b8117c3a55a8ae973eca4b1e192e4
Alex Males <alexandru.males@gmail.com>
2024-09-21 15:50:23 +0300

    added pancake swap deps

--- src/Catapoolt.sol
+++ src/Catapoolt.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.25;
 
 // Import OpenZeppelin's IERC20 and SafeERC20 libraries
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Catapoolt {
     using SafeERC20 for IERC20;
