02b98e22eb3c22cba081e5ecf37284ee6026f485
Alex Males <alexandru.males@gmail.com>
2024-09-23 07:46:02 +0300

    Brevis handler with reward multiplier logic

--- src/Catapoolt.sol
+++ src/Catapoolt.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.25;
 // Import OpenZeppelin's IERC20 and SafeERC20 libraries
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 import {PoolKey} from "pancake-v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "pancake-v4-core/src/types/PoolId.sol";
 import {ICLPoolManager} from "pancake-v4-core/src/pool-cl/interfaces/ICLPoolManager.sol";
 import {CLBaseHook} from "./CLBaseHook.sol";
 
-contract Catapoolt /* is CLBaseHook */ {
+import "brevis-sdk/apps/framework/BrevisApp.sol";
+import "brevis-sdk/interface/IBrevisProof.sol";
+
+contract Catapoolt is BrevisApp, Ownable {
     using SafeERC20 for IERC20;
     using PoolIdLibrary for PoolKey;
 
@@ -41,6 +45,13 @@ contract Catapoolt /* is CLBaseHook */ {
         uint256 claimedAt;
     }
 
+    struct Offering {
+        address currency;
+        uint256 amount;
+        PoolId poolId;
+        uint256 multiplier;
+    }
+
     Campaign[] public campaigns;
 
     uint256 public campaignId;
@@ -55,8 +66,60 @@ contract Catapoolt /* is CLBaseHook */ {
         uint256 endsAt
     );
 
+    bytes32 public vkHash;
 
-    constructor(ICLPoolManager _poolManager) /* CLBaseHook(_poolManager) */ {}
+    mapping(address => Offering[]) internal offerings;
+    mapping(address => uint256) internal offeringLengths;
+    mapping(address => mapping(PoolId => uint256)) internal ogMultipliers;
+
+    constructor(ICLPoolManager _poolManager, IBrevisProof _brevisProof) BrevisApp(address(_brevisProof)) Ownable(msg.sender) {
+
+    }
+
+    function setVkHash(bytes32 _vkHash) external onlyOwner {
+        vkHash = _vkHash;
+    }
+
+    function handleProofResult(
+        bytes32 _vkHash,
+        bytes calldata _appCircuitOutput
+    ) internal override {
+        require(vkHash == _vkHash, "invalid vk");
+        (address[] memory ogAddresses, address[] memory currencies, uint256[] memory amounts) = decodeOutput(_appCircuitOutput);
+
+        // TODO: Reset all OG multipliers
+
+        // Save OG multipliers on the corresponding pools
+        for (uint256 i = 0; i < ogAddresses.length; i++) {
+            address ogAddress = ogAddresses[i];
+            address currency = currencies[i];
+            uint256 amount = amounts[i];
+            for (uint256 j = 0; j < offeringLengths[currency]; j++) {
+                Offering storage offering = offerings[currency][j];
+                if (amount >= offering.amount) {
+                    ogMultipliers[ogAddress][offering.poolId] = offering.multiplier;
+                }
+            }
+        }
+    }
+
+    function decodeOutput(
+        bytes calldata output
+    ) internal pure returns (address[] memory, address[] memory, uint256[] memory) {
+        uint256 numEntries = output.length / 72;
+        address[] memory ogAddresses = new address[](numEntries);
+        address[] memory tokenAddresses = new address[](numEntries);
+        uint256[] memory amounts = new uint256[](numEntries);
+
+        for (uint256 i = 0; i < numEntries; i++) {
+            ogAddresses[i] = address(bytes20(output[i * 72: i * 72 + 20]));
+            tokenAddresses[i] = address(bytes20(output[i * 72 + 20: i * 72 + 40]));
+            amounts[i] = uint256(bytes32(output[i * 72 + 40: i * 72 + 72]));
+        }
+
+        return (ogAddresses, tokenAddresses, amounts);
+    }
+
 
     ////////////////////////////////////////
     // HOOK RELATED FUNCTIONS             //    
